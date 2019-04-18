f6857f05cec8ee7f9e6efd87d3f6660a1d611593
83au88 <brsherman88@gmail.com>
2019-04-17 20:16:04 -0700

    First commit.

---
+++ src/main.js
@@ -0,0 +1,32 @@
+$(document).ready(() => {
+  // Toggle header background visibility when mobile menu is shown
+  const navButton = document.querySelector('.navButton');
+  const input = document.getElementById('menuToggle');
+  navButton.addEventListener('click', () => {
+    if (!input.checked) {
+      document.querySelector('.header').classList.add('hide');
+    } else if (input.checked) {
+      document.querySelector('.header').classList.remove('hide');
+    }
+  });
+
+  // Highlight current page with jquery
+  $(function(){
+    $('a').each(function() {
+      if ($(this).prop('href') === window.location.href) {
+        $(this).addClass('current');
+      }
+    });
+  });
+});
+
+/*
+
+$('.navButton').on('click', () => {
+  if ($('input').is(':checked')) {
+    $('.header').addClass('hide');
+  } else if ($('input').is(':not(:checked)')) {
+    $('.header').removeClass('hide');
+  }
+});
+*/
