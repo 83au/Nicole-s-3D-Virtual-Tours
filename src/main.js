6c54ff5fc8d85e9dc32c5480bc0bd59f2fc6089e
83au88 <brsherman88@gmail.com>
2019-05-03 23:32:15 -0700

    Adjust services page.

--- src/main.js
+++ src/main.js
@@ -2,11 +2,9 @@ $(document).ready(() => {
 
   // Toggle header background visibility when mobile menu is shown
   const navButton = document.querySelector('.navButton');
-
   navButton.addEventListener('click', () => {
     document.querySelector('.header').classList.toggle('hide');
   });
-
 
   // Highlight current page with jquery
   $(function() {
@@ -17,7 +15,7 @@ $(document).ready(() => {
     });
   });
 
-  // Lazy load images
+  // Lazy load images using remote cdn
   var lazyLoad = new LazyLoad({
       elements_selector: ".lazy",
   });
