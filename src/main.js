078c06b0fa88d86189814daf241f2f19a9d474b1
83au88 <brsherman88@gmail.com>
2019-09-14 17:35:56 -0700

    Add promotional video.

--- src/main.js
+++ src/main.js
@@ -1,36 +1,32 @@
 const main = () => {
-
   // TODO: Add loading animation
-
   $(window).on('load', () => {
+    console.clear();
     console.log("PAGE FULLY LOADED");
     // TODO: Remove loading animation
   });
 
-  // Set copyright date
-  let date = new Date();
-  date = date.getFullYear();
-  $('footer span').append(date);
 
-  // toggle header background visibility when mobile menu is shown
+  // Set copyright date
+  const date = new Date();
+  const currentYear = date.getFullYear();
+  $('footer span').append(currentYear);
+
+  // Toggle header background visibility when mobile menu is clicked
   $('.navButton').click(() => $('.header').toggleClass('hide'));
 
-  // highlight current page
-  $('a').each(function() {
-    if ($(this).prop('href') === window.location.href) {
-      $(this).addClass('current');
-    }
-  });
 
-  // smooth scroll for safari and ios browsers
+  // Smooth scroll for safari and ios browsers
   $('.gallery-main__nav--link').smoothScroll();
+
 
   // Lazy load dollhouse images using remote cdn
   var lazyLoad = new LazyLoad({
     elements_selector: ".lazy",
   });
 
-  // slick
+
+  // Slick
   $('.stills').slick({
     dots: true,
     infinite: true,
@@ -55,5 +51,6 @@ const main = () => {
     ]
   });
 };
+
 
 $(document).ready(main);
