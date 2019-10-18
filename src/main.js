5292e96ebbb1c86fa0efb65f554363c8d7568874
83au88 <brsherman88@gmail.com>
2019-10-18 00:49:54 -0700

    Refactor & add loading animation.

--- src/main.js
+++ src/main.js
@@ -1,26 +1,18 @@
 const main = () => {
-  // TODO: Add loading animation
-  $(window).on('load', () => {
-    console.clear();
-    console.log("PAGE FULLY LOADED");
-    // TODO: Remove loading animation
-  });
-
 
   // Set copyright date
-  const date = new Date();
-  const currentYear = date.getFullYear();
-  $('footer span').append(currentYear);
+  $('footer span').append(new Date().getFullYear());
+
 
   // Toggle header background visibility when mobile menu is clicked
   $('.navButton').click(() => $('.header').toggleClass('hide'));
 
 
   // Smooth scroll for safari and ios browsers
-  $('.gallery-main__nav--link').smoothScroll();
+  $('.gallery-main__nav-link').smoothScroll();
 
 
-  // Lazy load dollhouse images using remote cdn
+  // Lazy load images using remote cdn
   var lazyLoad = new LazyLoad({
     elements_selector: ".lazy",
   });
@@ -50,7 +42,15 @@ const main = () => {
       }
     ]
   });
+
+
+  // Remove loading animation when page finishes loading
+  $(window).on('load', () => {
+    // console.clear();
+    console.log("PAGE FULLY LOADED");
+    $('.loader-container').addClass('remove');
+  });
 };
 
-
-$(document).ready(main);
+// Initialize
+$(main);
