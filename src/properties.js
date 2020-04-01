dadd155baf9cbf54963d289dfffbe178c6979ce4
Beau Sherman <brsherman88@gmail.com>
2020-03-31 20:30:21 -0700

    Create pagination functions.

---
+++ src/properties.js
@@ -0,0 +1,21 @@
+const properties = [
+  {
+    id: 'two-story-house',
+    title: 'Two Story House',
+    matterportLink: 'https://my.matterport.com/show/?m=y3GUmkCHCeK',
+    stills: [
+      {
+        id: '2-story-foyer',
+        title: 'Foyer',
+
+        popWebpSrcPath: './assets/2-story-stills-webp/foyer-1500w.webp',
+        popSrcPath: './assets/2-story-stills/foyer-1500w.jpg',
+        sectionLink: '#2-story-stills',
+
+        thumbWebpSrcPath: './assets/2-story-stills-webp/foyer-1500w.webp',
+        thumbSrcPath: './assets/2-story-stills/foyer-700w.jpg',
+      }
+    ]
+  }
+
+]
