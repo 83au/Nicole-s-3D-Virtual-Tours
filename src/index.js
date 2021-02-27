18924c0006cf16ef809e81d6c4388bb1fa8526c3
Beau Sherman <brsherman88@gmail.com>
2021-02-27 13:26:29 -0800

    Increase size of footer text and remove duplicate copyright year

--- src/index.js
+++ src/index.js
@@ -1,28 +1,26 @@
-import virtualTours from './gallery-data/virtual-tours/virtual-tours';
-import photosData from './gallery-data/photos-data/photos-data';
-import { 
+import virtualTours from "./gallery-data/virtual-tours/virtual-tours";
+import photosData from "./gallery-data/photos-data/photos-data";
+import {
   makeVirtualTourSection,
   makeStillsSection,
   activateAccordion,
-} from './helpers';
-import './scss/main.scss';
-
+} from "./helpers";
+import "./scss/main.scss";
 
 const main = () => {
   // Remove loading animation
-  $('.loader-container').addClass('remove');
-  
+  $(".loader-container").addClass("remove");
+
   // Set copyright date
-  $('footer span').append(new Date().getFullYear());
+  // $('footer span').append(new Date().getFullYear());
 
   // Toggle header background visibility when mobile menu is clicked
-  $('.navButton').click(() => $('.header').toggleClass('hide'));
+  $(".navButton").click(() => $(".header").toggleClass("hide"));
 
   // Initialize accordion functionality
-  $('.accordion').click(activateAccordion);
+  $(".accordion").click(activateAccordion);
 
-
-  // Home page slick 
+  // Home page slick
   const VidsSlickConfig = {
     dots: true,
     infinite: true,
@@ -30,17 +28,14 @@ const main = () => {
     slidesToShow: 1,
     slidesToScroll: 1,
   };
-  $('.video-slider').slick(VidsSlickConfig);
-
+  $(".video-slider").slick(VidsSlickConfig);
 
   // Smooth scroll for safari and ios browsers
-  $('.gallery-main__nav-link').smoothScroll();
-
+  $(".gallery-main__nav-link").smoothScroll();
 
   // Insert first property into DOM
-  const $galleryMain = $('.gallery-main');
+  const $galleryMain = $(".gallery-main");
   $galleryMain.html(makeVirtualTourSection(virtualTours[0]));
-
 
   const ImagesSlickConfig = {
     dots: true,
@@ -53,17 +48,17 @@ const main = () => {
         breakpoint: 800,
         settings: {
           slidesToShow: 2,
-          slidesToScroll: 2
-        }
+          slidesToScroll: 2,
+        },
       },
       {
         breakpoint: 670,
         settings: {
           slidesToShow: 1,
-          slidesToScroll: 1
-        }
-      }
-    ]
+          slidesToScroll: 1,
+        },
+      },
+    ],
   };
 
   function imagesInit() {
@@ -73,21 +68,24 @@ const main = () => {
     });
 
     // Slick
-    $('#stills').slick(ImagesSlickConfig);
+    $("#stills").slick(ImagesSlickConfig);
   }
   imagesInit();
 
-
   // Add link event listener
-  $('.gallery-main__nav').click(evt => {
-    if (evt.target.tagName !== 'A') return;
-    if (evt.target.href.includes('#stills')) {
-      const stillsObj = photosData.find(obj => evt.target.href.includes(obj.id));
+  $(".gallery-main__nav").click((evt) => {
+    if (evt.target.tagName !== "A") return;
+    if (evt.target.href.includes("#stills")) {
+      const stillsObj = photosData.find((obj) =>
+        evt.target.href.includes(obj.id)
+      );
       $galleryMain.html(makeStillsSection(stillsObj));
       imagesInit();
       return;
     }
-    const property = virtualTours.find(prop => evt.target.href.includes(prop.id));
+    const property = virtualTours.find((prop) =>
+      evt.target.href.includes(prop.id)
+    );
     $galleryMain.html(makeVirtualTourSection(property));
     imagesInit();
   });
