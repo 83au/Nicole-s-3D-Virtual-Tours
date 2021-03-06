d529ad88cca050995abf1c4bddac02da37dd6460
Beau Sherman <brsherman88@gmail.com>
2021-03-06 14:18:15 -0800

    Add dynamic copyright date and services style adjustments

--- src/global.js
+++ src/global.js
@@ -1,6 +1,6 @@
 const global = () => {
   // Set copyright date
-  // $('footer span').append(new Date().getFullYear());
+  $("footer span").append(new Date().getFullYear());
 
   // Toggle header background visibility when mobile menu is clicked
   $(".navButton").on("click", () => $(".header").toggleClass("hide"));
