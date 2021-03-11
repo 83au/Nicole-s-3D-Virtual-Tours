416a6d11b76ab4efa45bcf572b2c9b2869589424
Beau Sherman <brsherman88@gmail.com>
2021-03-10 19:16:53 -0800

    Add lazyload to setup page

--- src/setup.js
+++ src/setup.js
@@ -1,4 +1,25 @@
 import global from "./global";
 import "./scss/pages/setup.scss";
 
-$(global);
+const setup = () => {
+  $(global);
+
+  const setupVideo = document.querySelector(".setup-main__vid-container");
+  setupVideo.addEventListener("click", () => {
+    setupVideo.innerHTML = `
+      <div class="setup-main__resp-container">
+        <iframe
+          width="560"
+          height="315"
+          class="resp-iframe"
+          src="https://www.youtube.com/embed/lNRGh5udg24"
+          frameborder="0"
+          allow="autoplay; accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowfullscreen
+        ></iframe>
+      </div>
+    `;
+  });
+};
+
+$(setup);
