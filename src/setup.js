5572da9dc0694f7ad94705e75e9822f5b039b2b6
Beau Sherman <brsherman88@gmail.com>
2021-03-14 18:11:38 -0700

    Add autoplay to setup page

--- src/setup.js
+++ src/setup.js
@@ -1,18 +1,18 @@
-import global from "./global";
-import "./scss/pages/setup.scss";
+import global from './global';
+import './scss/pages/setup.scss';
 
 const setup = () => {
   $(global);
 
-  const setupVideo = document.querySelector(".setup-main__vid-container");
-  setupVideo.addEventListener("click", () => {
+  const setupVideo = document.querySelector('.setup-main__vid-container');
+  setupVideo.addEventListener('click', () => {
     setupVideo.innerHTML = `
       <div class="setup-main__resp-container">
         <iframe
           width="560"
           height="315"
           class="resp-iframe"
-          src="https://www.youtube.com/embed/lNRGh5udg24"
+          src="https://www.youtube.com/embed/lNRGh5udg24?rel=0&autoplay=1"
           frameborder="0"
           allow="autoplay; accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
           allowfullscreen
